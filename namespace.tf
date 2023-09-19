4e2610cb67a552ca6256df8c16f67a92bc219365
Deen Muhammad <40787426+deenMuhammad@users.noreply.github.com>
2023-09-19 17:40:10 +0900

    Add ns if not existing

--- namespace.tf
+++ namespace.tf
@@ -1,4 +1,7 @@
+data "kubernetes_all_namespaces" "allns" {}
+
 resource "kubernetes_namespace" "alb" {
+  count = (contains(data.kubernetes_all_namespaces.allns.namespaces, var.k8s_namespace)) ? 0 : 1
   metadata {
     name = var.k8s_namespace
   }
